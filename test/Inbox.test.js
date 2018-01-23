1b2058b5735890428cdaaa12ad7c3b05b3dc2d36
Daniel Lehrner <daniel.lehrner@pwc-digital.ch>
2018-01-23 07:23:00 +0100

    reformated code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,7 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
-const { interface, bytecode } = require('../compile');
+const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
@@ -13,8 +13,8 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
-        .send({ from: accounts[0], gas: '1000000' });
+        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
+        .send({from: accounts[0], gas: '1000000'});
     inbox.setProvider(provider);
 });
 
@@ -29,7 +29,7 @@ describe('Inbox', () => {
     });
 
     it('can change the message', async () => {
-        await inbox.methods.setMessage('new message').send({ from: accounts[0] });
+        await inbox.methods.setMessage('new message').send({from: accounts[0]});
         const message = await inbox.methods.message().call();
         assert.equal(message, 'new message');
     });
